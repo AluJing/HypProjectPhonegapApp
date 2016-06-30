5ed3f3a5bab8810fa871ee6a070f5e09e26ca618
fabiomeraviglia <fabiomeraviglia@users.noreply.github.com>
2016-06-30 12:28:22 +0200

    Update networking-fetch.js

--- assets/js/networking-fetch.js
+++ assets/js/networking-fetch.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-        $.get("networkingFetch.php", 
+        $.get("http://jingalmera.altervista.org/website/networkingFetch.php", 
                 { categoria: "networking" }, function(data, status){
     
 		document.getElementById("devicesElement").innerHTML = data;
