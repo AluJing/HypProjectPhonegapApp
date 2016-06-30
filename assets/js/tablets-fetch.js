06429d678d33e9c9d13a224e50a62004c1f9b1ab
fabiomeraviglia <fabiomeraviglia@users.noreply.github.com>
2016-06-30 12:26:54 +0200

    Update tablets-fetch.js

--- assets/js/tablets-fetch.js
+++ assets/js/tablets-fetch.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-        $.get("tabletFetch.php", 
+        $.get("http://jingalmera.altervista.org/website/tabletFetch.php", 
                 { categoria: "tablet" }, function(data, status){
     
 		document.getElementById("devicesElement").innerHTML = data;
